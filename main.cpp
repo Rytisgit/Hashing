372a794c92d983071c18726ec97427ee53fef45e
rytisgit <prytisx@gmai.com>
2019-10-04 01:10:39 +0300

    add all testing

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <set>
+#include <algorithm>
+#include <random>
 #include "RytisMasterClassOnHashingAlgorithms.h"
+#include "timer.h"
 
 std::string readFile(std::string string);
+void testDuplicates();
+void testSimilarity();
+std::string random_string( size_t length )
+{
+    thread_local static std::mt19937 rg{
+            static_cast<unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count())};
+    thread_local static std::uniform_int_distribution<int> pick(0, goodCharsLength);
+    auto randchar = []() -> char {return goodChars[ pick(rg) % goodCharsLength ];};
+    std::string str(length,0);
+    std::generate_n( str.begin(), length, randchar );
+    return str;
+}
 int main(int argc, char *argv[]) {
-    if(argv[3][0]=='1'){
+    if(argv[1][1]!='t'){
+    if(argv[1][1]=='l'){
         std::string line;
         std::string location {};
-        location += "../";
-        location+=argv[2];
-        location+=".txt";
-        std::ifstream myfile(location);
+        std::ifstream myfile(argv[2]);
+        double time = 0;
         while ( std::getline(myfile, line))
         {
+            Timer t;
+            hash(line);
             std::cout << hash(line);
             std::cout << '\n';
+            time+=t.elapsed();
         }
+        std::cout << time;
     }
     else{
         if(argv[1][1]=='s')
@@ -23,6 +42,11 @@ int main(int argc, char *argv[]) {
         else{
             std::cout<<hash(readFile(argv[2]));
         }
+    }}
+    else{
+        testSimilarity();
+        std::cout<<std::endl;
+        testDuplicates();
     }
     return 0;
 }
@@ -31,14 +55,50 @@ std::string readFile(std::string string)
 {
     std::ostringstream sstream;
     std::string line;
-    std::string location {};
-    location += "../";
-    location+=string;
-    location+=".txt";
-    std::ifstream myfile(location);
-    while ( std::getline(myfile, line))
-    {
-        sstream<<line;
-    }
+    std::ifstream myfile(string);
+    while ( std::getline(myfile, line))sstream<<line;
     return sstream.str();
+}
+void testDuplicates(){
+    std::set<std::string> hashes;
+    std::set<std::string> strings;
+    double totalTime = 0;
+    double intervalTime = 0;
+    int printInterval = 1e5;
+    int duplicate = 0;
+    for (int i = 1; i <= 1e6; i++){
+        auto string = random_string(6);
+        Timer start;
+        std::string res = hash(string);
+        double currentTime = start.elapsed();
+        totalTime += currentTime;
+        intervalTime += currentTime;
+        auto p = hashes.insert(res);
+        auto t = strings.insert(string);
+        if (!p.second)//on fail to insert hash
+            duplicate++;
+        if (!t.second)
+            std::cout << "same string generated twice:"<<string<<std::endl;
+        if (i%printInterval==0){
+            std::cout << "Hashes: " << i << ", Duplicates: " << duplicate << " Hashes/s: " << printInterval/intervalTime << std::endl;
+            intervalTime = 0;
+        }
+    }
+    std::cout<<"Total time:" <<totalTime<< std::endl;
+}
+void testSimilarity(){
+    double min = 1, max = 0;
+    double total = 0;
+    for (int i = 1; i <= 1e5; i++){
+        double cmp = HashComparer(random_string(5)+"a", random_string(5)+"A");
+        if (cmp < min)
+            min = cmp;
+        if (cmp > max)
+            max = cmp;
+        total += cmp;
+    }
+    std::cout << "Similarity ratings: " << std::endl;
+    std::cout << "Min: " << min << std::endl;
+    std::cout << "Max: " << max << std::endl;
+    std::cout << "Avg: " << total/1e5 << std::endl;
 }
